7f22e4da46d7395dc55271314c7aec19ef7e22f4
Ni4ka1991 <astahovaveronica@gmail.com>
2024-01-01 19:04:41 +0200

    add some code

---
+++ app.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+print(globals())
+print(f"__name__ >>> {__name__}")
+
+def sum_test( 2, 6 ):
+    return x + y
+
+def main():
+    print( "Great! We turn ON main func" )
+
+
+if __name__ == "__main__":
+    print( "Good job, Veronika!!!" )
+    main()
+else:
+    print( f"main >>> {__name__}" )
